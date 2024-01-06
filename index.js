cf00a8bd225d1c4dd14248dba7a4919e6762e4b9
AbdillahHamzahAli <hamzahjmbg14@gmail.com>
2024-01-06 21:54:51 +0700

    add multer

--- index.js
+++ index.js
@@ -7,21 +7,29 @@ import CONFIG from "./config/environments.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import TagRoute from "./routes/TagRoute.js";
 import PostRoute from "./routes/PostRoute.js";
+import multerStorage from "./middleware/multerStorage.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = CONFIG.port || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
+// app.use(multerStorage);
 
 // Route
 app.use(AuthRoute);
 app.use(TagRoute);
 app.use(PostRoute);
+app.use("/storage/images", express.static(path.join(__dirname, "/storage/images")));
 
 // Database Connection
 mongoose
